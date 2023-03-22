2c0adb98b29d269855281c33f0ae0754efe2bfb7
quinnmike82 <87664191+quinnmike82@users.noreply.github.com>
2023-03-22 15:57:58 +0700

    update order

--- TicketOnline/Model/Order.cs
+++ TicketOnline/Model/Order.cs
@@ -16,7 +16,7 @@ namespace TicketOnline.Data
         [ForeignKey("CustomerId")]
         public virtual Customer? Customer { get; set; }
         [DataType(DataType.Currency)]
-        public decimal Total { get; set; } = 0;
+        public SqlMoney Total { get; set; } = 0;
         public bool Status { get; set; }
         public DateTime? CreateAt { get; set; } = DateTime.UtcNow;
         //one to many
